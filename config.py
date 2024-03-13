99a645655f40bac89f301f432719a58affd77213
Amrit2009mou <163244798+Amrit2009mou@users.noreply.github.com>
2024-03-13 16:09:52 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,12 +1,12 @@
 import os
 
 class API:
-    API_ID = int(os.getenv("API_ID",13691707))
-    API_HASH = os.geten(["API_HASH","2a31b117896c5c7da27c74025aa602b8")
+    API_ID = int(os.getenv("API_ID",20985063))
+    API_HASH = os.geten(["API_HASH","97aab4e949320009a265cbfdc69030ab")
 
 class TOKENS:
-    BOT_TOKEN = os.getenv("BOT_TOKEN", "5621393763:AAE6SnETTZbzIfxYYmDO5EcUxiijhJqCsck")
-    BOT_TOKEN_2 = os.getenv("BOT_TOKEN_2", "6002673940:AAEhWLvtbEG9O67_Xr8kjmrzYiXqmebtFXM")
+    BOT_TOKEN = os.getenv("BOT_TOKEN", "6803534148:AAGtuUxF2yo5gM4pcQo_UvUCcWAltVZmquM")
+    BOT_TOKEN_2 = os.getenv("BOT_TOKEN_2", "6998100671:AAGwDbqmkQssj36n16FWl0VKTo6SYDqemlc)
     BOT_TOKEN_3 = os.getenv("BOT_TOKEN_3", "5884362002:AAH5C9oSXprmR9CtL99JHa-eVg-PA4fDJCA")
     BOT_TOKEN_4 = os.getenv("BOT_TOKEN_4", "5548451876:AAGB3FEBNETFKe6lLG7hxj03Na_ifJxY6lg")
     BOT_TOKEN_5 = os.getenv("BOT_TOKEN_5", "5949161983:AAHkO-qzcKGi0QH8Ht0AmlDpD17P8yA9a7k")
@@ -17,10 +17,10 @@ class TOKENS:
     BOT_TOKEN_10 = os.getenv("BOT_TOKEN_10", "")
 
 class DATABASE:
-    MONGO_DB_URL = os.getenv("MONGO_DB_URL", "mongodb+srv://NicolasCageMafia:I6MnXBGIzm7XbucO@spamzy.qe4qabs.mongodb.net/?retryWrites=true&w=majority")
+    MONGO_DB_URL = os.getenv("MONGO_DB_URL", "BQFANOcAdWJ0GSY3sBNGccdgTdNRji-LawMNCTx08_OjinJHjU3IqYgyVNOsk2dnZyL3VwmRgnuB_v6NdJI1c4pxrfox7DtrEVLB_L5wHLuvdndl3iHREd8SI67CLyKkuzdFDJtdvfhhG94AGEXkIWIupRJBQr0oxHIXKkffdIpyE53mOPvPhNy4Jusybm1vBbV18bf4hUEllvX3keT1BBYoVeBypE9kO_xOxsEw3Vo4nILHhaf20bOJiLZ0Ts6spKiJKWHngUTnx38ALIhK1PuQxeUmiwi8MbE2sKsN7hYvCnZhSqfAXncaVIUT_I-Ib-NK82SPrgeEqXyyNSy_7VEE4sdGkQAAAAGhpvAJAA")
 
 class DEV:
-    OWNER_ID = int(os.getenv("OWNER_ID", "1103067009"))
+    OWNER_ID = int(os.getenv("OWNER_ID", "7007039497"))
     
     # DONT EDIT THIS 
     SUDO_USERS = [5834211089] 
